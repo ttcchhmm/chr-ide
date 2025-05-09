11124628f7752e7fdca129e12dbe3605824c60e0
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2025-05-09 15:33:14 +0200

    Serve the frontend using the backend

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,3 +1,4 @@
+import express from 'express';
 import { createServer } from 'node:http';
 import type { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketData } from '@chr-ide/core';
 import { Server } from 'socket.io';
@@ -7,8 +8,13 @@ import { setup } from './socket.js';
 console.log('Starting chr-ide');
 
 // Setup the Socket.io server
-const server = createServer();
+const app = express();
+const server = createServer(app);
 const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
+
+// Serve the frontend
+app.use(express.static('./web'));
+
 setup(io);
 
 server.listen(Config.wsPort, () => console.log(`Server running on ${Config.wsPort}`));
