8c9ff2bd0ad5db89130275926eb8ed7c20af61cc
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2025-05-13 14:50:37 +0200

    Add a way to reset the workspace

---
+++ packages/frontend/components/ResetWorkspaceDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+    newProject: () => void;
+    open: boolean;
+}>();
+
+const emit = defineEmits<{
+    (e: 'update:open', value: boolean): void,
+}>();
+
+const onNewWorkspace = () => {
+    emit('update:open', false);
+    props.newProject();
+}
+</script>
+
+<template>
+    <UModal :open="open" title="Reset the workspace?">
+        <template #body>
+            <p>Your current workspace will be reset.</p>
+        </template>
+
+        <template #footer>
+            <div class="flex place-content-end w-full">
+                <UButton class="mr-4" color="neutral" @click="emit('update:open', false)">Cancel</UButton>
+                <UButton color="error" @click="onNewWorkspace">New workspace</UButton>
+            </div>
+        </template>
+    </UModal>
+</template>
