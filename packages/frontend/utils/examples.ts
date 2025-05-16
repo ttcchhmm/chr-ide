b4606616a5e48b024e2fab7443bea3f0e00bc17c
Gyrehio <117647406+Gyrehio@users.noreply.github.com>
2025-05-16 13:51:12 +0200

    Add examples (Syracuse + GCD)

--- packages/frontend/utils/examples.ts
+++ packages/frontend/utils/examples.ts
@@ -3,15 +3,22 @@
  */
 export default [
     {
-        name: 'Example 1',
-        code: 'I lost',
-        constraints: [],
-        variables: [],
+        name: 'Syracuse\'s conjecture',
+        code:
+`<chr_constraint> syracuse(?unsigned long int, ?unsigned long int)
+stop @ syracuse(1LU, C) <=> C %= 0;;\neven_case @ syracuse(R,C) <=> R % 2 == 0 | syracuse(R / 2,C1), C %= C1 + 1;;
+odd_case @ syracuse(R,C) <=> R % 2 == 1 | syracuse(3 * R + 1,C1), C %= C1 + 1;;`,
+        constraints: ["syracuse(10,0)"],
+        variables: [{"constraint": "syracuse", "position": 2, "value": ""}],
     },
     {
-        name: 'Example 2',
-        code: 'I lost again',
-        constraints: [],
-        variables: [],
+        name: 'Greatest Common Divisor',
+        code: 
+`<chr_constraint> gcd(+unsigned long int), res(?unsigned long int)
+gcd1 @ gcd(0ul) <=> success();;
+gcd2 @ gcd(N) \ gcd(M) <=> N <= M | gcd(M % N);;
+res  @ gcd(N) \ res(M) <=> M %= N;;`,
+        constraints: ["gcd(12)", "gcd(8)"],
+        variables: [{"constraint" : "res", "position": 1, "value": ""}],
     },
 ] as Example[]; 
